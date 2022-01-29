6614e0f3d3fd475a3f94adbad27021dfda87cd02
federico-palma <fedepal9@gmail.com>
2022-01-29 12:05:38 +0100

    Added Navbar

---
+++ src/Navbar.js
@@ -0,0 +1,18 @@
+const Navbar = () => {
+    return (
+    <nav className="navbar">
+        <div className="header">
+            <img id="navbar-logo" src={require("./images/navbar-cat.png")} alt="logo-cat.png"/>
+            <h2>Cat Memes</h2>
+        </div>
+        
+        <div className="navbar-links">
+            <button className="navbar-link">Home</button>
+            <button className="navbar-link">Cat 1</button>
+            <button className="navbar-link">Cat 2</button>
+        </div>
+    </nav>
+    );
+}
+ 
+export default Navbar;
